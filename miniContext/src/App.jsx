9b93dbd023b8005f697c08604ef9d1bc655f9d57
Ajeem12 <ajeembeg98.com>
2024-06-27 13:59:14 +0530

    context api project

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>React with Me and Fun</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  );
+}
+
+export default App;
