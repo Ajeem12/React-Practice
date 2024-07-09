c11097b2359fb87c4604d2455fe0edba267346cf
Ajeem12 <ajeembeg98.com>
2024-07-09 17:19:48 +0530

    Add Signup and AuthLayout

--- Blog/src/components/Signup.jsx
+++ Blog/src/components/Signup.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
+import authService from "../appwrite/auth";
 import { Link, useNavigate } from "react-router-dom";
-import { login as authLogin } from "../store/authSlice";
+import { login } from "../store/authSlice";
 import { Button, Input, Logo } from "./index";
 import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
-function Login() {
+
+function Signup() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
   const [error, setError] = useState("");
 
-  const login = async (data) => {
+  const signup = async (data) => {
     setError("");
     try {
-      const session = await authService.login(data);
-      if (session) {
-        const userData = await authService.getCurrentUser();
-        if (userData) dispatch(authLogin(userData));
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const userDta = await authService.getCurrentUser();
+        if (userDta) dispatch(login(userDta));
         navigate("/");
       }
     } catch (error) {
@@ -26,9 +27,9 @@ function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div
-        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl border border-black/10`}
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-[100px]">
@@ -36,41 +37,51 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in your account
+          Sign up to create account
         </h2>
-        <p className=" mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
-        {error && <p className="text-red-500 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
             <Input
-              label="Email"
+              label="Full Name: "
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
               type="email"
-              placeholder="Email"
               {...register("email", {
                 required: true,
                 validate: {
                   matchPatern: (value) =>
                     /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                    "Email address must be a valid email address",
+                    "Email address must be a valid address",
                 },
               })}
             />
             <Input
-              label="Password:"
+              label="Password: "
               type="password"
               placeholder="Enter your password"
-              {...register("password", { required: true })}
+              {...register("password", {
+                required: true,
+              })}
             />
             <Button type="submit" className="w-full">
-              Sign In
+              Create Account
             </Button>
           </div>
         </form>
@@ -79,4 +90,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -3,12 +3,24 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import Select from "./Select"
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard"
+
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
- 
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    Signup,
+    Login,
+    PostCard
 }
